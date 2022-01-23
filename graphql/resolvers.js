6ebef9339883f2bcd9b15e3dd9d5b68ea82d972a
sannimdev <inegg.apps@gmail.com>
2022-01-23 22:10:02 +0900

    0.11 Delete Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-import { getMovies, getById, addMovie } from './db';
+import { getMovies, getById, addMovie, deleteMovie } from './db';
 // 요건 나중에 Database로 빼야겠지?
 // Query를 resolve(해결)한다.
 // Rest API에서는 URL이 있엇다면 GraphQL에서는 Resolver가 있다.
@@ -9,6 +9,7 @@ const resolvers = {
     },
     Mutation: {
         addMovie: (_, { name, score }) => addMovie(name, score),
+        deleteMovie: (_, { id }) => deleteMovie(id),
     },
 };
 
