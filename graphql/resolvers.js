1539e96989fbdb46ba1cb806a43df8e3c5903ed3
sannimdev <inegg.apps@gmail.com>
2022-01-23 21:47:30 +0900

    0.8 Creating Queries with Arguments

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,11 @@
-import { people } from './db';
+import { getById, people } from './db';
 // 요건 나중에 Database로 빼야겠지?
 // Query를 resolve(해결)한다.
 // Rest API에서는 URL이 있엇다면 GraphQL에서는 Resolver가 있다.
 const resolvers = {
     Query: {
         people: () => people,
-        // person: () => getById() ,
+        person: (_, { id }) => getById(id),
     },
 };
 
