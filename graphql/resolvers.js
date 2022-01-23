e1a691d2a4e3e4d66c7e7ccc6b23368bb8ac4b45
sannimdev <inegg.apps@gmail.com>
2022-01-23 21:16:41 +0900

    0.6 Extending the Schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,14 @@
+// 요건 나중에 Database로 빼야겠지?
+const nicolas = {
+    name: 'Nicolas',
+    age: 18,
+    gender: 'female',
+};
 // Query를 resolve(해결)한다.
 // Rest API에서는 URL이 있엇다면 GraphQL에서는 Resolver가 있다.
 const resolvers = {
     Query: {
-        name: () => 'nicolas',
+        person: () => nicolas,
     },
 };
 
